cb9affcb3c9fb901d510f854d91e5065e4492288
shi_zj@suixingpay.com <shi_zj@suixingpay.com>
2020-02-11 21:26:46 +0800

    测试一下

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -14,7 +14,7 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, world!')
+      .expect('hi, world! ok')
       .expect(200);
   });
 });
